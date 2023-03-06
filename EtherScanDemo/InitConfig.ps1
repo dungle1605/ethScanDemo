ed463ab1a3433a6c9b299470b7428aeb4687c382
Le Tien Dung <dugle1605@gmail.com>
2023-03-06 12:22:43 +0700

    Add script for generating Secrets file

---
+++ EtherScanDemo/InitConfig.ps1
@@ -0,0 +1,3 @@
+dotnet user-secrets set APIKey Y2XFK6HMF7HBK2C49NVYUYPX1P6GFX9HFS
+dotnet user-secrets set ConnectionString "Server=127.0.0.1;Port=3306;Database=ethscan;Uid=root;Pwd="
+dotnet user-secrets set LogsPath "D:\Projects\EtherScanDemo\TracingLogs.txt"
